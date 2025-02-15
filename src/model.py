a87e7ee0e7da091c00924b0c7c76cebd2be0ee9e
Szymon Pawłowski <szymon.pawlowski.social@gmail.com>
2025-02-15 19:17:52 +0100

    Updated model file

---
+++ src/model.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+class LinearRegressionModel:
+    def __init__(self), learning_rate = 0.01, iterations = 1000):
+        self.learning_rate = learning_rate
+        self.iterations = iterations
+        self.weights = None
+        self.bias = None
+    
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+        
+        for i in range(self.iterations):
+            y_predicted - np.dot(X, self.weights) + self.bias
+            dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
+            db = (1 / n_samples) * np.sum(y_predicted - y)
+    
+    def predict(self, X):
+        return np.dot(X, self.weights) + self.bias
