Added gradient creation, corrected syntax error

src/model.py
@@ -13,9 +13,12 @@ class LinearRegressionModel:
         self.bias = 0
         
         for i in range(self.iterations):
-            y_predicted - np.dot(X, self.weights) + self.bias
+            y_predicted = np.dot(X, self.weights) + self.bias
             dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
             db = (1 / n_samples) * np.sum(y_predicted - y)
+            
+            self.weights -= self.learning_rate * dw
+            self.bias -= self.learning_rate * db
     
     def predict(self, X):
         return np.dot(X, self.weights) + self.bias