91c5d7106a13cfc3efa33f8145fa8fc9bee3c791
Szymon Pawłowski <szymon.pawlowski.social@gmail.com>
2025-02-17 16:41:07 +0100

    Corrected errors in 'fit' function

--- src/model.py
+++ src/model.py
@@ -1,12 +1,13 @@
 import numpy as np
 
+
 class LinearRegressionModel:
     def __init__(self, learning_rate = 0.01, iterations = 1000):
         self.learning_rate = learning_rate
         self.iterations = iterations
         self.weights = None
         self.bias = None
-    
+        
     def fit(self, X, y):
         n_samples, n_features = X.shape
         self.weights = np.zeros(n_features)
@@ -14,11 +15,13 @@ class LinearRegressionModel:
         
         for i in range(self.iterations):
             y_predicted = np.dot(X, self.weights) + self.bias
+            
             dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
             db = (1 / n_samples) * np.sum(y_predicted - y)
             
             self.weights -= self.learning_rate * dw
             self.bias -= self.learning_rate * db
-    
+            
     def predict(self, X):
         return np.dot(X, self.weights) + self.bias
+    
