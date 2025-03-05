8087e3b8b7f24bd7ca2cf9ace6f722decb101f67
Szymon Pawłowski <szymon.pawlowski.social@gmail.com>
2025-03-05 16:51:33 +0100

    Deleted unused library

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from src import LinearRegressionModel, mean_squared_error, plot_predictions
 
 # RANDOM DATA GENERATION
@@ -17,7 +16,7 @@ y_pred = model.predict(X)
 
 # MSE ERROR
 mse = mean_squared_error(y, y_pred)
-print(f"Błąd MSE: {mse:.4f}")
+print(f"MSE error: {mse:.4f}")
 
 # PLOT
 plot_predictions(X, y, y_pred)
