8d5dcab429e82525780c89c177fd64ab8c9d1b64
Szymon Pawłowski <szymon.pawlowski.social@gmail.com>
2025-03-05 16:50:16 +0100

    Changed matplotlib backend

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
+import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
+matplotlib.use('TkAgg')
 
 def mean_squared_error(y_true, y_pred):
     return np.mean((y_true - y_pred) ** 2)
@@ -8,4 +10,7 @@ def plot_predictions(X, y_true, y_pred):
     plt.scatter(X, y_true, color = "blue", label = "True values")
     plt.plot(X, y_pred, color = "red", label = "Prediction model")
     plt.legend()
+    plt.draw()
+    plt.pause(0.001)
+    plt.draw()
     plt.show()
