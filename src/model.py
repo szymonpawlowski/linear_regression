c8f11ded7c1d4c58f54075207a8fe1ea77accde3
Szymon Pawłowski <szymon.pawlowski.social@gmail.com>
2025-03-20 12:54:39 +0100

    Deleted empty lines

--- src/model.py
+++ src/model.py
@@ -7,21 +7,20 @@ class LinearRegressionModel:
         self.epochs = epochs
         self.weights = None
         self.bias = None
-        
+
     def fit(self, X, y):
         n_samples, n_features = X.shape
         self.weights = np.zeros(n_features)
         self.bias = 0
-        
+
         for i in range(self.epochs):
             y_predicted = np.dot(X, self.weights) + self.bias
-            
+
             dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
             db = (1 / n_samples) * np.sum(y_predicted - y)
-            
+
             self.weights -= self.learning_rate * dw
             self.bias -= self.learning_rate * db
-            
+
     def predict(self, X):
         return np.dot(X, self.weights) + self.bias
-    
