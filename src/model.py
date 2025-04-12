5dc43b75b1c7f74a229fcacbca6bbc15251331a1
Szymon Pawłowski <szymon.pawlowski.social@gmail.com>
2025-04-12 21:55:09 +0200

    Big revamp - model types, added simple model

--- src/model.py
+++ src/model.py
@@ -1,27 +1,26 @@
 import numpy as np
+import random
 
-
-class LinearRegressionModel:
+class SimpleLinearRegression:
     def __init__(self, learning_rate = 0.01, epochs = 1000):
         self.learning_rate = learning_rate
         self.epochs = epochs
-        self.weights = None
-        self.bias = None
+        self.weights = random.uniform(-1, 1)
+        self.bias = random.uniform(-1, 1)
         
-    def fit(self, X, y):
-        n_samples, n_features = X.shape
-        self.weights = np.zeros(n_features)
-        self.bias = 0
-        
-        for i in range(self.epochs):
-            y_predicted = np.dot(X, self.weights) + self.bias
-            
-            dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
-            db = (1 / n_samples) * np.sum(y_predicted - y)
-            
+    def predict(self, X):
+        return [self.weights * x + self.bias for x in X]
+    
+    def fit(self, X, y_true, on_epoch = None):
+        n = len(X)
+        for epoch in range(self.epochs):
+            y_pred = self.predict(X)
+            dw = (-2 / n) * sum((y_true[i] - y_pred[i]) * X[i] for i in range(n))
+            db = (-2 / n) * sum((y_true[i] - y_pred[i]) for i in range(n))
             self.weights -= self.learning_rate * dw
             self.bias -= self.learning_rate * db
-            
-    def predict(self, X):
-        return np.dot(X, self.weights) + self.bias
-    
+            if on_epoch:
+                on_epoch(epoch, self.predict(X))
+
+    def mse(self, y_true, y_pred):
+        return sum((yt - yp)**2 for yt, yp in zip(y_true, y_pred)) / len(y)
