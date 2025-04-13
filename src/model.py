01000a018d22d757ad08ba1ec9137db284491b13
Szymon Pawłowski <szymon.pawlowski.social@gmail.com>
2025-04-13 12:43:40 +0200

    Removed random data generation

--- src/model.py
+++ src/model.py
@@ -1,13 +1,12 @@
 import numpy as np
-import random
 
 
 class SimpleLinearRegression:
     def __init__(self, learning_rate = 0.01, epochs = 1000):
         self.learning_rate = learning_rate
         self.epochs = epochs
-        self.weights = random.uniform(-1, 1)
-        self.bias = random.uniform(-1, 1)
+        self.weights = 0.0
+        self.bias = 0.0
 
     def fit(self, X, y_true, on_epoch = None):
         n = len(X)
@@ -29,21 +28,23 @@ class AdvancedLinearRegression:
         self.learning_rate = learning_rate
         self.epochs = epochs
         self.weights = None
-        self.bias = None
+        self.bias = 0.0
 
-    def fit(self, X, y_true):
+    def fit(self, X, y_true, on_epoch = None):
         X = np.array(X)
         y_true = np.array(y_true)
         n_samples, n_features = X.shape
         self.weights = np.zeros(n_features)
         self.bias = 0
 
-        for _ in range(self.epochs):
+        for epoch in range(self.epochs):
             y_pred = np.dot(X, self.weights) + self.bias
             dw = (1 / n_samples) * np.dot(X.T, (y_pred - y_true))
             db = (1 / n_samples) * np.sum(y_pred - y_true)
             self.weights -= self.learning_rate * dw
             self.bias -= self.learning_rate * db
+            if on_epoch:
+                on_epoch(epoch, self.predict(X))
 
     def predict(self, X):
         X = np.array(X)
