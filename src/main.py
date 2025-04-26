a1d9c907d8cd94dbf2c60a22c6c921ac7911dd5a
Szymon Pawłowski <szymon.pawlowski.social@gmail.com>
2025-04-26 19:50:14 +0200

    Added main file structure

--- src/main.py
+++ src/main.py
@@ -1,1 +1,22 @@
-from src.io_handler import get_user_input
+from io_handler import get_data, get_learning_parameters
+from model import get_model
+from utils import mean_squared_error
+
+
+def main():
+    X, y_true = get_data()
+
+    learning_rate, epochs = get_learning_parameters()
+
+    model = get_model(X, learning_rate=learning_rate, epochs=epochs)
+
+    model.fit(X, y_true)
+    y_pred = model.predict(X)
+    mse = mean_squared_error(y_true, y_pred)
+
+    print(8*"="+" Training complete "+"="*8)
+    print(f"Mean Squared Error = {mse}")
+
+
+if __name__ == '__main__':
+    main()
