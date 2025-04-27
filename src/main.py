2d6087a7673547262ecc369e36738dd6a245c17c
Szymon Pawłowski <szymon.pawlowski.social@gmail.com>
2025-04-27 16:54:41 +0200

    Added visualization

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from io_handler import get_data, get_learning_parameters
 from model import get_model
-from utils import mean_squared_error
+from utils import mean_squared_error, plot_regression
+import matplotlib.pyplot as plt
+plt.ion()
 
 
 def main():
@@ -10,7 +12,11 @@ def main():
 
     model = get_model(X, learning_rate=learning_rate, epochs=epochs)
 
-    model.fit(X, y_true)
+    def on_epoch(epoch, model):
+        y_pred = model.predict(X)
+        plot_regression(X, y_true, epoch, y_pred)
+
+    model.fit(X, y_true, on_epoch=on_epoch)
     y_pred = model.predict(X)
     mse = mean_squared_error(y_true, y_pred)
 
