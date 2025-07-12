9fcd4f7eb9914407af959ca1ebca1fae6827fe4c
Szymon Pawłowski <szymon.pawlowski.social@gmail.com>
2025-07-12 14:46:46 +0200

    Added title to window app

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -6,7 +6,9 @@ class LinearRegressionApp:
         super().__init__()
         self.window = tk.Tk()
         self.window.title("Linear Regression")
-        self.window.geometry("800x800")
+        self.window.geometry("800x600")
+        self.welcome_label = tk.Label(self.window, text = "LINEAR REGRESSION", font = ("Consolas", 20), justify = "center")
+        self.welcome_label.pack(anchor = "n")   # label alignment to center - north anchor
         self.window.mainloop()
 
 
