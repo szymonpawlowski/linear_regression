98c6c7f68d2499e88744884a5e146407265c6b74
Szymon Pawłowski <szymon.pawlowski.social@gmail.com>
2025-07-12 16:07:01 +0200

    Added exit button function

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -10,19 +10,18 @@ class LinearRegressionApp:
         self.create_start_screen()
         self.window.mainloop()
 
+    def start_app(self):
+        
+    def exit_app(self):
+        self.window.quit()
+
     def create_start_screen(self):
-        self.welcome_label = tk.Label(self.window, text = "LINEAR REGRESSION", font = ("Consolas", 20), justify = "center")
+        self.welcome_label = tk.Label(self.window, text = "LINEAR REGRESSION", font = ("Consolas", 20, "bold"), justify = "center")
         self.welcome_label.pack(anchor = "n")   # label alignment to center - north anchor
         self.start_button = tk.Button(self.window, text = "START")
-        self.start_button.pack(pady = 10, anchor = "n")
-        self.exit_button = tk.Button(self.window, text = "EXIT")
+        self.start_button.pack(pady = 10, anchor = "n", command = self.start_app)
+        self.exit_button = tk.Button(self.window, text = "EXIT", command = self.exit_app)
         self.exit_button.pack(anchor = "n")
-
-    #def start_app(self):
-        
-
-    #def exit_app(self):
-        
 
 
 app = LinearRegressionApp()
