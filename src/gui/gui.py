19229f54f632b1e5f58896c51daf263398e8c193
Szymon Pawłowski <szymon.pawlowski.social@gmail.com>
2025-07-12 14:31:04 +0200

    Created empty app window

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -5,3 +5,9 @@ class LinearRegressionApp:
     def __init__(self):
         super().__init__()
         self.window = tk.Tk()
+        self.window.title("Linear Regression")
+        self.window.geometry("800x800")
+        self.window.mainloop()
+
+
+app = LinearRegressionApp()
