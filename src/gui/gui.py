d4a1f111210ff9a80aa25b9d3963ad4744e9e3a5
Szymon Pawłowski <szymon.pawlowski.social@gmail.com>
2025-08-01 16:34:40 +0200

    Added dynamic display of menus

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -5,7 +5,7 @@ class LinearRegressionApp:
     def __init__(self):
         self.window = tk.Tk()
         self.window.title("Linear Regression")
-        self.window.geometry("800x600")
+        self.window.geometry("1200x800")
         self.create_start_menu()
         self.window.mainloop()
 
@@ -21,15 +21,18 @@ class LinearRegressionApp:
         self.data_selection_frame.columnconfigure(2, weight=1)
 
         self.random_data_button = tk.Button(self.data_selection_frame, text="RANDOM DATA")
-        self.random_data_button.grid(row=0, column=0, sticky="we", padx=5)
+        self.random_data_button.grid(row=0, column=0, padx=5)
+
         self.user_data_button = tk.Button(self.data_selection_frame, text="USER DATA")
-        self.user_data_button.grid(row=0, column=1, sticky="we")
+        self.user_data_button.grid(row=0, column=1)
+
         self.file_data_button = tk.Button(self.data_selection_frame, text="FILE DATA")
-        self.file_data_button.grid(row=0, column=2, sticky="we", padx=5)
+        self.file_data_button.grid(row=0, column=2,padx=5)
+
         self.return_start_menu_button = tk.Button(self.data_selection_frame, text="RETURN", command=self.return_start_menu)
         self.return_start_menu_button.grid(row=1, column=1, pady=5)
 
-        self.data_selection_frame.pack(fill="x")
+        self.data_selection_frame.place(relx=0.5, rely=0.5)
 
     def start_app(self):
         # clearing window and creating mode selection
@@ -43,14 +46,18 @@ class LinearRegressionApp:
     def create_start_menu(self):
         self.main_frame = tk.Frame(self.window)
         self.main_frame.columnconfigure(0, weight=1)
+
         self.welcome_label = tk.Label(self.main_frame, text="LINEAR REGRESSION", font=("Consolas", 20, "bold"), justify="center")
         self.welcome_label.grid(row=0, column=0)
+
         self.start_button = tk.Button(self.main_frame, text="START", command=self.start_app)
         self.start_button.grid(row=1, column=0, padx=200, pady=10, sticky="we")
+
         self.exit_button = tk.Button(self.main_frame, text="EXIT", command=self.exit_app)
         self.exit_button.grid(row=2, column=0, padx=200, sticky="we")
+
         self.start_menu_widgets = [self.welcome_label, self.start_button, self.exit_button]
-        self.main_frame.pack(fill="x")
+        self.main_frame.place(relx=0.5, rely=0.5, anchor="center")
 
 
 app = LinearRegressionApp()
