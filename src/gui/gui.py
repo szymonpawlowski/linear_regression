fb840eec39d0d3e88872fac96f242107ef1ebde5
Szymon Pawłowski <szymon.pawlowski.social@gmail.com>
2025-08-03 13:32:53 +0200

    Big overhaul of layout - menubar

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -6,56 +6,52 @@ class LinearRegressionApp:
         self.window = tk.Tk()
         self.window.title("Linear Regression")
         self.window.geometry("1600x1000")
-        self.create_start_menu()
+        self.menubar = tk.Menu(self.window)
+        self.window.config(menu=self.menubar)
+        self.create_data_menubar()
+        self.create_options_menubar()
         self.window.mainloop()
 
     def return_start_menu(self):
-        self.data_selection_frame.destroy()
+        self.random_data_button.destroy()
+        self.user_data_button.destroy()
         self.return_start_menu_button.destroy()
         self.create_start_menu()
 
     def create_data_selection_menu(self):
-        self.data_selection_frame = tk.Frame(self.window)
-        self.data_selection_frame.columnconfigure(0, weight=1)
-        self.data_selection_frame.columnconfigure(1, weight=1)
-        self.data_selection_frame.columnconfigure(2, weight=1)
+        self.random_data_button = tk.Button(self.sidebar, text="RANDOM DATA")
+        self.random_data_button.grid(row=1, column=1, padx=5)
 
-        self.random_data_button = tk.Button(self.data_selection_frame, text="RANDOM DATA")
-        self.random_data_button.grid(row=0, column=0, padx=5)
+        self.user_data_button = tk.Button(self.sidebar, text="USER DATA")
+        self.user_data_button.grid(row=2, column=1)
 
-        self.user_data_button = tk.Button(self.data_selection_frame, text="USER DATA")
-        self.user_data_button.grid(row=0, column=1)
+        self.file_data_button = tk.Button(self.sidebar, text="FILE DATA")
+        self.file_data_button.grid(row=3, column=1, padx=5)
 
-        self.file_data_button = tk.Button(self.data_selection_frame, text="FILE DATA")
-        self.file_data_button.grid(row=0, column=2,padx=5)
-
-        self.return_start_menu_button = tk.Button(self.data_selection_frame, text="RETURN", command=self.return_start_menu)
-        self.return_start_menu_button.grid(row=1, column=1, pady=5)
-
-        self.data_selection_frame.place(relx=0.5, rely=0.5)
+        self.return_start_menu_button = tk.Button(self.sidebar,
+                                                  text="RETURN",
+                                                  command=self.return_start_menu)
+        self.return_start_menu_button.grid(row=4, column=1, pady=5)
 
     def start_app(self):
-        # clearing window and creating mode selection
-        for widget in self.start_menu_widgets:
-            widget.grid_forget()
         self.create_data_selection_menu()
 
     def exit_app(self):
         self.window.quit()
 
     def create_start_menu(self):
-        self.main_frame = tk.Frame(self.window)
-        self.main_frame.columnconfigure(0, weight=1)
+        self.main_sidebar =  tk.Frame(self.window)
+        self.main_sidebar.columnconfigure(0, weight=1)
 
-        self.welcome_label = tk.Label(self.main_frame,
-                                      text="LINEAR\n REGRESSION ",
-                                      font=("Consolas", 20, "bold"),
-                                      justify="center",
-                                      relief="raised",
-                                      bd=5)
-        self.welcome_label.grid(row=0, column=0, sticky="we")
+        self.title_label = tk.Label(self.main_sidebar,
+                                    text="LINEAR\n REGRESSION ",
+                                    font=("Consolas", 20, "bold"),
+                                    justify="center",
+                                    relief="raised",
+                                    bd=5)
+        self.title_label.grid(row=0, column=0, sticky="we")
 
-        self.start_button = tk.Button(self.main_frame,
+        self.start_button = tk.Button(self.main_sidebar,
                                       text="START",
                                       height=2,
                                       font=("Consolas", 15),
@@ -64,7 +60,7 @@ class LinearRegressionApp:
                                       command=self.start_app)
         self.start_button.grid(row=1, column=0, sticky="we")
 
-        self.exit_button = tk.Button(self.main_frame,
+        self.exit_button = tk.Button(self.main_sidebar,
                                      text="EXIT",
                                      height=2,
                                      font=("Consolas", 15),
@@ -73,14 +69,30 @@ class LinearRegressionApp:
                                      command=self.exit_app)
         self.exit_button.grid(row=2, column=0, sticky="we")
 
-        self.empty_label = tk.Label(self.main_frame,
+        self.empty_label = tk.Label(self.main_sidebar,
                                     relief="raised",
                                     bd=5,
                                     height=10000)
         self.empty_label.grid(row=3, column=0, sticky="we")
 
-        self.start_menu_widgets = [self.welcome_label, self.start_button, self.exit_button, self.empty_label]
-        self.main_frame.place(x=0, y=0, anchor="nw")
+        self.start_menu_widgets = [self.title_label, self.start_button, self.exit_button, self.empty_label]
+        self.main_sidebar.place(x=0, y=0, anchor="nw")
+
+    def create_data_menubar(self):
+        self.data_menu = tk.Menu(self.menubar)
+        self.data_menu.add_command(label="Generate random data")
+        self.data_menu.add_command(label="Input data manually")
+        self.data_menu.add_command(label="Import data from file")
+        self.menubar.add_cascade(label="Data",
+                                 menu=self.data_menu,
+                                 underline=0)
+
+    def create_options_menubar(self):
+        self.options_menu = tk.Menu(self.menubar)
+        self.menubar.add_cascade(label="Options",
+                                 menu=self.options_menu,
+                                 underline=0)
+        
 
 
 app = LinearRegressionApp()
