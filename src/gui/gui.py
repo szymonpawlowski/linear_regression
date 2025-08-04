b473c853b918809c87f6bbdfaee9af17af0407f0
Szymon Pawłowski <szymon.pawlowski.social@gmail.com>
2025-08-04 16:02:49 +0200

    Deleted not used functions

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -13,48 +13,8 @@ class LinearRegressionApp:
         self.create_exit_menubar()
         self.window.mainloop()
 
-
     def exit_app(self):
         self.window.quit()
-
-    def create_start_menu(self):
-        self.main_sidebar =  tk.Frame(self.window)
-        self.main_sidebar.columnconfigure(0, weight=1)
-
-        self.title_label = tk.Label(self.main_sidebar,
-                                    text="LINEAR\n REGRESSION ",
-                                    font=("Consolas", 20, "bold"),
-                                    justify="center",
-                                    relief="raised",
-                                    bd=5)
-        self.title_label.grid(row=0, column=0, sticky="we")
-
-        self.start_button = tk.Button(self.main_sidebar,
-                                      text="START",
-                                      height=2,
-                                      font=("Consolas", 15),
-                                      relief="raised",
-                                      bd=5,
-                                      command=self.start_app)
-        self.start_button.grid(row=1, column=0, sticky="we")
-
-        self.exit_button = tk.Button(self.main_sidebar,
-                                     text="EXIT",
-                                     height=2,
-                                     font=("Consolas", 15),
-                                     relief="raised",
-                                     bd=5,
-                                     command=self.exit_app)
-        self.exit_button.grid(row=2, column=0, sticky="we")
-
-        self.empty_label = tk.Label(self.main_sidebar,
-                                    relief="raised",
-                                    bd=5,
-                                    height=10000)
-        self.empty_label.grid(row=3, column=0, sticky="we")
-
-        self.start_menu_widgets = [self.title_label, self.start_button, self.exit_button, self.empty_label]
-        self.main_sidebar.place(x=0, y=0, anchor="nw")
 
     def create_data_menubar(self):
         self.data_menu = tk.Menu(self.menubar)
@@ -67,9 +27,7 @@ class LinearRegressionApp:
 
     def create_options_menubar(self):
         self.options_menu = tk.Menu(self.menubar)
-        self.menubar.add_cascade(label="Options",
-                                 menu=self.options_menu,
-                                 underline=0)
+        self.menubar.add_cascade(label="Options")
 
     def create_exit_menubar(self):
         self.exit_menu = tk.Menu(self.menubar)
