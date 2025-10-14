d88af350c716ab192af22c396419d4a7ae3d084c
Szymon Pawłowski <szymon.pawlowski.social@gmail.com>
2025-10-14 21:13:04 +0200

    Added data source file with random data getter

---
+++ src/regression/data_sources.py
@@ -0,0 +1,12 @@
+import random
+
+
+def get_random_data(n, xmin, xmax, wmin, wmax, bmin, bmax):
+    data = []
+    for _ in range(n):
+        X = random.uniform(wmin, wmax)
+        weight = random.uniform(wmin, wmax)
+        bias = random.uniform(bmin, bmax)
+        y_pred = weight * X + bias
+        data.append((X, y_pred))
+    return data
